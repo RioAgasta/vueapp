505b02bd70f7dacbb719743a14ec82079c8a5108
RioAgasta <rioagasta@gmail.com>
2022-10-22 16:39:41 +0700

    Added Icons and Rest1 without image edit

--- src/views/ListPage.vue
+++ src/views/ListPage.vue
@@ -12,13 +12,14 @@
           <div class="row">
             <div class="col-md-4" v-for="product in products" :key="product.id">
               <div class="card mt-4 p-4">
-                <center>
+                <div class="row">
                   <img
                     :src="`http://localhost:8000/storage/${product.image}`"
                     width="200"
                     height="200"
+                    style="object-fit: contain;"
                   />
-                </center>
+                </div>
                 <div class="card-body" align="right">
                   <h5 class="card-title">{{ product.title }}</h5>
                   <p class="card-text">Harga Rp. {{ product.price }}</p>
